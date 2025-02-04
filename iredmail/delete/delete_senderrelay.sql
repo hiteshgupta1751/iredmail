cb5219ba88be4215bc74496b7e9a176b55170e58
hiteshgupta1751 <hiteshgupta898@gmail.com>
2025-02-04 06:33:09 +0000

    deleye

--- iredmail/delete/delete_senderrelay.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM sender_relayhost WHERE account = 'eva.goldstein@example.com';
-DELETE FROM sender_relayhost WHERE account = 'edith.erickson@example.com';
--- iredmail/delete/delete_users.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM mailbox WHERE username = 'eva.goldstein@example.com';
-DELETE FROM forwardings WHERE address = 'eva.goldstein@example.com' OR forwarding = 'eva.goldstein@example.com';
-DELETE FROM mailbox WHERE username = 'edith.erickson@example.com';
-DELETE FROM forwardings WHERE address = 'edith.erickson@example.com' OR forwarding = 'edith.erickson@example.com';
